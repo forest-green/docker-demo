30e77ff0dfa19c2f35e615ca107bb510359b3f46
dongzhiwen <dongzhiwen@cn.wilmar-intl.com>
2021-02-19 10:48:47 +0800

    这是一个测试文件

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 /*
- * @Description: Do not edit
+ * @Description: test
  * @Author: dongzhiwen
  * @since: 2021-02-19 10:32:37
  * @LastAuthor: dongzhiwen
- * @lastTime: 2021-02-19 10:32:37
+ * @lastTime: 2021-02-19 10:48:30
  */
+
+console.log('这是一个测试文件')
